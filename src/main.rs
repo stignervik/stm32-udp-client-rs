c244c350a11e04cd84736ebf7b2541d3638c548d
Stig Nervik <stig.nervik@carrier.com>
2024-03-07 13:14:52 +0100

    Clean up.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_net::tcp::client::{TcpClient, TcpClientState};
+// use embassy_net::tcp::client::{TcpClient, TcpClientState};
 use embassy_net::udp::{PacketMetadata, UdpSocket};
 use embassy_net::{Ipv4Address, Ipv4Cidr, Stack, StackResources};
 use embassy_stm32::eth::generic_smi::GenericSMI;
@@ -11,9 +11,9 @@ use embassy_stm32::eth::{Ethernet, PacketQueue};
 use embassy_stm32::peripherals::ETH;
 use embassy_stm32::rng::Rng;
 use embassy_stm32::{bind_interrupts, eth, peripherals, rng, Config};
-use embassy_time::Timer;
-use embedded_io_async::Write;
-use embedded_nal_async::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpConnect};
+// use embassy_time::Timer;
+// use embedded_io_async::Write;
+// use embedded_nal_async::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpConnect};
 use heapless::Vec;
 use rand_core::RngCore;
 use static_cell::StaticCell;
@@ -163,140 +163,5 @@ async fn main(spawner: Spawner) -> ! {
             // Handle error: buffer too small
             info!("Buffer is too small to add extra text.");
         }
-
-        /*
-        // You need to start a server on the host machine, for example: `nc -l 8000`
-        // The IP address is the IP address of the host machine.
-        let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(172, 16, 201, 12), 8000));
-
-        info!("connecting...");
-        let r = client.connect(addr).await;
-        if let Err(e) = r {
-            info!("connect error: {:?}", e);
-            Timer::after_secs(1).await;
-            continue;
-        }
-        let mut connection = r.unwrap();
-        info!("connected!");
-        loop {
-            let r = connection.write_all(b"Hello\n").await;
-            if let Err(e) = r {
-                info!("write error: {:?}", e);
-                break;
-            }
-            Timer::after_secs(1).await;
-        }
-        */
     }
 }
-
-/*
-#![no_std]
-#![no_main]
-
-use clap::Parser;
-use embassy_executor::{Executor, Spawner};
-use embassy_net::udp::{PacketMetadata, UdpSocket};
-use embassy_net::{Config, Ipv4Address, Ipv4Cidr, Stack, StackResources};
-use embassy_net_tuntap::TunTapDevice;
-use heapless::Vec;
-use log::*;
-use rand_core::{OsRng, RngCore};
-use static_cell::StaticCell;
-
-
-#[derive(Parser)]
-#[clap(version = "1.0")]
-struct Opts {
-    /// TAP device name
-    #[clap(long, default_value = "tap0")]
-    tap: String,
-    /// use a static IP instead of DHCP
-    #[clap(long)]
-    static_ip: bool,
-}
-
-
-#[embassy_executor::task]
-async fn net_task(stack: &'static Stack<TunTapDevice>) -> ! {
-    stack.run().await
-}
-
-#[embassy_executor::task]
-async fn main_task(spawner: Spawner) {
-    let opts: Opts = Opts::parse();
-
-    // Init network device
-    let device = TunTapDevice::new(&opts.tap).unwrap();
-
-    // Choose between dhcp or static ip
-    let config = if opts.static_ip {
-        Config::ipv4_static(embassy_net::StaticConfigV4 {
-            address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 69, 2), 24),
-            dns_servers: Vec::new(),
-            gateway: Some(Ipv4Address::new(192, 168, 69, 1)),
-        })
-    } else {
-        Config::dhcpv4(Default::default())
-    };
-
-    // Generate random seed
-    let mut seed = [0; 8];
-    OsRng.fill_bytes(&mut seed);
-    let seed = u64::from_le_bytes(seed);
-
-    // Init network stack
-    static STACK: StaticCell<Stack<TunTapDevice>> = StaticCell::new();
-    static RESOURCES: StaticCell<StackResources<3>> = StaticCell::new();
-    let stack = &*STACK.init(Stack::new(
-        device,
-        config,
-        RESOURCES.init(StackResources::<3>::new()),
-        seed,
-    ));
-
-    // Launch network task
-    spawner.spawn(net_task(stack)).unwrap();
-
-    // Then we can use it!
-    let mut rx_meta = [PacketMetadata::EMPTY; 16];
-    let mut rx_buffer = [0; 4096];
-    let mut tx_meta = [PacketMetadata::EMPTY; 16];
-    let mut tx_buffer = [0; 4096];
-    let mut buf = [0; 4096];
-
-    let mut socket = UdpSocket::new(
-        stack,
-        &mut rx_meta,
-        &mut rx_buffer,
-        &mut tx_meta,
-        &mut tx_buffer,
-    );
-    socket.bind(9400).unwrap();
-
-    loop {
-        let (n, ep) = socket.recv_from(&mut buf).await.unwrap();
-        if let Ok(s) = core::str::from_utf8(&buf[..n]) {
-            info!("ECHO (to {}): {}", ep, s);
-        } else {
-            info!("ECHO (to {}): bytearray len {}", ep, n);
-        }
-        socket.send_to(&buf[..n], ep).await.unwrap();
-    }
-}
-
-static EXECUTOR: StaticCell<Executor> = StaticCell::new();
-
-fn main() {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Debug)
-        .filter_module("async_io", log::LevelFilter::Info)
-        .format_timestamp_nanos()
-        .init();
-
-    let executor = EXECUTOR.init(Executor::new());
-    executor.run(|spawner| {
-        spawner.spawn(main_task(spawner)).unwrap();
-    });
-}
-*/
